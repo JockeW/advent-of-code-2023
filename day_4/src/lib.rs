bde856d0001c2969c2ac99bca70272bfcda5a8bc
JockeW <wilhelmsson.joakim@gmail.com>
2023-12-04 13:47:10 +0100

    WIP Day 4

--- day_4/src/lib.rs
+++ day_4/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::{BTreeMap, HashMap};
+
 pub fn part_one(input: &str) -> usize {
     let mut all_card_points: Vec<usize> = Vec::new();
 
@@ -40,27 +42,107 @@ pub fn part_one(input: &str) -> usize {
 }
 
 pub fn part_two(input: &str) -> usize {
+    let mut cards: BTreeMap<u32, &str> = BTreeMap::new();
+    let mut cards_left: Vec<u32> = Vec::new();
+
     for line in input.trim().split('\n') {
-        //
+        let card = line.split(":").collect::<Vec<&str>>();
+        let card_id_str = card[0];
+        //let card_id_parts = card_id_str.split(" ").collect::<Vec<&str>>();
+        let card_id = card_id_str
+            .split(" ")
+            .collect::<Vec<&str>>()
+            .last()
+            .unwrap()
+            .parse()
+            .unwrap();
+        let card_numbers = card[1];
+        cards.insert(card_id, card_numbers);
+        cards_left.push(card_id);
+    }
+    //println!("Hash: {:?}", cards);
+    //println!("Cards left start: {:?}", cards_left);
+
+    for (card, values) in cards.iter() {
+        let mut count: usize = cards_left
+            .iter()
+            .filter(|&c| c == card)
+            .collect::<Vec<&u32>>()
+            .len();
+        println!("card: {}. count: {}", card, count);
+
+        while count > 0 {
+            let matching_numbers = check_card(values); //TODO: Don't need to do this everytime. One time per card should be enough, than we know the value here
+            //println!("Matches: {}", matching_numbers);
+            let start = card + 1;
+            let end = start + matching_numbers;
+            for i in start..end {
+                cards_left.push(i);
+            }
+
+            cards_left.sort();
+            //println!("Cards left: {:?}", cards_left);
+            count -= 1;
+        }
     }
 
-    1
+    cards_left.sort();
+    //println!("Cards left: {:?}", cards_left);
+
+    cards_left.len()
+}
+
+fn check_card(card_numbers: &str) -> u32 {
+    let mut card_points: u32 = 0;
+    //println!("Numbers: {:?}", card_numbers);
+
+    let numbers = card_numbers.split("|").collect::<Vec<&str>>();
+    let winning_numbers: Vec<&str> = numbers[0]
+        .trim()
+        .split(" ")
+        .collect::<Vec<&str>>()
+        .iter()
+        .filter(|&&n| n != "")
+        .map(|n| *n)
+        .collect();
+
+    let my_numbers: Vec<&str> = numbers[1]
+        .trim()
+        .split(" ")
+        .collect::<Vec<&str>>()
+        .iter()
+        .filter(|&&n| n != "")
+        .map(|n| *n)
+        .collect();
+
+    for num in winning_numbers {
+        if my_numbers.contains(&num) {
+            card_points += 1;
+        }
+    }
+
+    card_points
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn example() {
-        assert_eq!(super::part_one(include_str!("example.txt")), 13);
-    }
-
-    #[test]
-    fn part_one() {
-        assert_eq!(super::part_one(include_str!("input.txt")), 23235);
-    }
+    // #[test]
+    // fn example() {
+    //     assert_eq!(super::part_one(include_str!("example.txt")), 13);
+    // }
 
     // #[test]
-    // fn part_two() {
-    //     assert_eq!(super::part_two(include_str!("input.txt")), 0);
+    // fn part_one() {
+    //     assert_eq!(super::part_one(include_str!("input.txt")), 23235);
     // }
+
+    // #[test]
+    // fn example2() {
+    //     assert_eq!(super::part_two(include_str!("example.txt")), 30);
+    // }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(super::part_two(include_str!("input.txt")), 0);
+    }
 }

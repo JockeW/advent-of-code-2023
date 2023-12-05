34957e315c1ecf878deba3bde1d5af6f75441554
JockeW <wilhelmsson.joakim@gmail.com>
2023-12-05 21:48:27 +0100

    Day 5 part one

--- day_5/src/lib.rs
+++ day_5/src/lib.rs
@@ -1,18 +1,84 @@
-pub fn part_one(input: &str) -> usize {
-    let seeds: Vec<u32> = Vec::new();
-    let seed_to_soil: Vec<[u32; 3]> = Vec::new();
-    let soil_to_fertilizer: Vec<[u32; 3]> = Vec::new();
-    let fertilizer_to_water: Vec<[u32; 3]> = Vec::new();
-    let water_to_light: Vec<[u32; 3]> = Vec::new();
-    let light_to_temperature: Vec<[u32; 3]> = Vec::new();
-    let temperature_to_humidity: Vec<[u32; 3]> = Vec::new();
-    let humidity_to_location: Vec<[u32; 3]> = Vec::new();
+pub fn part_one(input: &str) -> u64 {
+    let lines = input
+        .trim()
+        .split('\n')
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|&l| l.trim())
+        .collect::<Vec<&str>>();
+    let seeds: Vec<u64> = lines[0].split(":").collect::<Vec<&str>>()[1]
+        .trim()
+        .split(" ")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|&s| s.parse::<u64>().unwrap())
+        .collect();
 
-    for line in input.trim().split('\n') {
-        let seed_line = line.split(":").collect::<Vec<&str>>()[1].trim();
-        seeds.append(seed_line.split(" ").collect::<Vec<&str>>().iter().map(|&s| s.parse()).collect());
+    let mut locations: Vec<u64> = Vec::new();
+
+    for seed in seeds {
+        let mut mapping_found = false;
+        let mut source = seed;
+        let mut destination = seed;
+        for (index, &line) in lines[2..lines.len()].iter().enumerate() {
+            if line.is_empty() || index == lines[2..lines.len()].len() - 1 {
+                if mapping_found == false {
+                    destination = source;
+                    println!("Destination (same as source): {}", destination);
+                }
+
+                continue;
+            }
+
+            if line
+                .chars()
+                .collect::<Vec<char>>()
+                .first()
+                .unwrap()
+                .is_digit(10)
+                == false
+            {
+                println!("New mapping: {}", line);
+                source = destination;
+                destination = 0;
+                mapping_found = false;
+            } else if mapping_found {
+                continue;
+            } else {
+                let map_numbers: Vec<u64> = line
+                    .split(" ")
+                    .collect::<Vec<&str>>()
+                    .iter()
+                    .map(|&m| m.parse::<u64>().unwrap())
+                    .collect();
+
+                let source_map = map_numbers[1];
+                let destination_map = map_numbers[0];
+                let map_range = map_numbers[2];
+
+                let source_in_map = source >= source_map && source <= source_map + map_range;
+                if !source_in_map {
+                    continue;
+                }
+
+                let difference = destination_map.abs_diff(source_map);
+
+                destination = if source_map < destination_map {
+                    source + difference
+                } else if source_map > destination_map {
+                    source - difference
+                } else {
+                    source
+                };
+
+                mapping_found = true;
+            }
+        }
+
+        locations.push(destination);
     }
-    0
+
+    *locations.iter().min().unwrap()
 }
 
 pub fn part_two(input: &str) -> usize {
@@ -29,10 +95,10 @@ mod tests {
         assert_eq!(super::part_one(include_str!("example.txt")), 35);
     }
 
-    // #[test]
-    // fn part_one() {
-    //     assert_eq!(super::part_one(include_str!("input.txt")), 0);
-    // }
+    #[test]
+    fn part_one() {
+        assert_eq!(super::part_one(include_str!("input.txt")), 26273516);
+    }
 
     // #[test]
     // fn part_two() {

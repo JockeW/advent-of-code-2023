c5041fbe1e8e10768effd85a77c14e25d971493f
JockeW <wilhelmsson.joakim@gmail.com>
2023-12-06 10:51:13 +0100

    Day 6

--- day_6/src/lib.rs
+++ day_6/src/lib.rs
@@ -19,14 +19,18 @@ pub fn part_one(input: &str) -> usize {
     for race in races {
         let mut ways_to_win: Vec<u32> = Vec::new();
         let mut button_press_time = 1;
+        let mut previous_distance = 0;
 
         while button_press_time <= race.0 {
             let distance_traveled = (race.0 - button_press_time) * button_press_time;
 
             if distance_traveled > race.1 {
                 ways_to_win.push(button_press_time);
+            } else if distance_traveled < previous_distance {
+                break;
             }
 
+            previous_distance = distance_traveled;
             button_press_time += 1;
         }
 
@@ -36,11 +40,39 @@ pub fn part_one(input: &str) -> usize {
     beat_record.iter().product()
 }
 
-pub fn part_two(input: &str) -> usize {
-    for line in input.trim().split('\n') {
-        //
+pub fn part_two(input: &str) -> u32 {
+    let lines: Vec<&str> = input.trim().split('\n').collect();
+
+    let time: u64 = lines[0].split(":").collect::<Vec<&str>>()[1]
+        .trim()
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
+
+    let distance = lines[1].split(":").collect::<Vec<&str>>()[1]
+        .trim()
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
+
+    let mut ways_to_win: u32 = 0;
+    let mut button_press_time = 1;
+    let mut previous_distance = 0;
+
+    while button_press_time <= time {
+        let distance_traveled = (time - button_press_time) * button_press_time;
+
+        if distance_traveled > distance {
+            ways_to_win += 1;
+        } else if distance_traveled < previous_distance {
+            break;
+        }
+
+        previous_distance = distance_traveled;
+        button_press_time += 1;
     }
-    0
+
+    ways_to_win
 }
 
 #[cfg(test)]
@@ -55,8 +87,13 @@ mod tests {
         assert_eq!(super::part_one(include_str!("input.txt")), 449550);
     }
 
-    // #[test]
-    // fn part_two() {
-    //     assert_eq!(super::part_two(include_str!("input.txt")), 0);
-    // }
+    #[test]
+    fn example2() {
+        assert_eq!(super::part_two(include_str!("example.txt")), 71503);
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(super::part_two(include_str!("input.txt")), 28360140);
+    }
 }

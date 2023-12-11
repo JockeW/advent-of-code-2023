e285778abd42444773e1475fd710077b2d6ea4f6
JockeW <wilhelmsson.joakim@gmail.com>
2023-12-11 20:45:45 +0100

    Day 9 completed

--- day_9/src/lib.rs
+++ day_9/src/lib.rs
@@ -49,11 +49,55 @@ pub fn part_one(input: &str) -> i32 {
     next_values.iter().sum()
 }
 
-pub fn part_two(input: &str) -> usize {
-    for line in input.trim().split('\n') {
-        //
+pub fn part_two(input: &str) -> i32 {
+    let histories: Vec<Vec<i32>> = input
+        .trim()
+        .split('\n')
+        .map(|s| {
+            s.trim()
+                .split(' ')
+                .map(|n| n.parse().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+
+    let mut next_values: Vec<i32> = Vec::new();
+
+    for history in histories {
+        let mut sequences: Vec<Vec<i32>> = Vec::new();
+        sequences.push(history);
+
+        loop {
+            let mut sequence: Vec<i32> = Vec::new();
+
+            for (i, n) in sequences.last().unwrap().iter().enumerate() {
+                if i == sequences.last().unwrap().len() - 1 {
+                    break;
+                }
+
+                sequence.push(sequences.last().unwrap()[i + 1] - n);
+            }
+
+            sequences.push(sequence);
+
+            if sequences.last().unwrap().iter().all(|&s| s == 0) {
+                break;
+            }
+        }
+
+        sequences.reverse();
+        let mut increase_by = 0;
+        for mut sequence in sequences {
+            let new_sequence_number = sequence.first().unwrap() - increase_by;
+            sequence.insert(0, new_sequence_number);
+
+            increase_by = new_sequence_number;
+        }
+
+        next_values.push(increase_by);
     }
-    0
+
+    next_values.iter().sum()
 }
 
 #[cfg(test)]
@@ -68,8 +112,13 @@ mod tests {
         assert_eq!(super::part_one(include_str!("input.txt")), 1953784198);
     }
 
-    // #[test]
-    // fn part_two() {
-    //     assert_eq!(super::part_two(include_str!("input.txt")), 0);
-    // }
+    #[test]
+    fn example2() {
+        assert_eq!(super::part_two(include_str!("example.txt")), 2);
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(super::part_two(include_str!("input.txt")), 957);
+    }
 }
